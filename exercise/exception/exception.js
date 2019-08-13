b2fa7730c64bac802cd8dee6f7cf36e7a0fd005d
schktjm <si2.o.mjt@gmail.com>
2019-08-13 16:38:51 +0900

    error handling

--- exercise/exception/exception.js
+++ exercise/exception/exception.js
@@ -2,15 +2,21 @@ class MultiplicatorError extends Error {}
 
 function primitiveMultiply(a, b) {
   if (Math.random() < 0.2) {
-    return a * b;
+    return a * b
   } else {
-    throw new MultiplicatorError("Oops...");
+    throw new MultiplicatorError('Oops...')
   }
 }
 
 function reliableMultiply(a, b) {
+  try {
+    return primitiveMultiply(a, b)
+  } catch (e) {
+    if (e instanceof MultiplicatorError) {
+      return reliableMultiply(a, b)
+    }
+  }
   // code here
-  return primitiveMultiply(a, b);
 }
 
-console.log(reliableMultiply(4, 4));
+console.log(reliableMultiply(4, 4))
