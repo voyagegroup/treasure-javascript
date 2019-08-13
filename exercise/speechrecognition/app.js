b5d22e3c8fee029bb3a8cccae35a289a9b352147
schktjm <si2.o.mjt@gmail.com>
2019-08-13 17:37:15 +0900

    event

--- exercise/speechrecognition/app.js
+++ exercise/speechrecognition/app.js
@@ -1,32 +1,36 @@
-const displayStatus = (t) => {
-	document.querySelector("#status").textContent = t;
+const displayStatus = t => {
+  document.querySelector('#status').textContent = t
 }
 
 const speechRecognition = () => {
-	if ("SpeechRecognition" in window) {
-	  return new SpeechRecognition();
-	} else if ("webkitSpeechRecognition" in window) {
-	  return new webkitSpeechRecognition();
-	} else {
-	  return undefined;
-	}
+  if ('SpeechRecognition' in window) {
+    return new SpeechRecognition()
+  } else if ('webkitSpeechRecognition' in window) {
+    return new webkitSpeechRecognition()
+  } else {
+    return undefined
+  }
 }
 
-const recognition = speechRecognition();
+const recognition = speechRecognition()
+recognition.continuous = true
+recognition.interimResults = true
 
-recognition.lang = "ja-JP";
+recognition.lang = 'ja-JP'
 recognition.onstart = function() {
-	displayStatus("音声認識スタート");
+  console.log('onaudiostart')
 }
 
 recognition.onresult = function(event) {
-	alert(event.results[0][0].transcript);
+  document.getElementsByTagName('textarea')[0].value +=
+    event.results[0][0].transcript
 }
 
 recognition.onerror = function(event) {
-  displayStatus(event.error);
+  displayStatus(event.error)
 }
 
-document.querySelector("#startSpeech").addEventListener("click", function(e) {
-	recognition.start();
-});
+document.querySelector('#startSpeech').addEventListener('click', function(e) {
+  recognition.start()
+  displayStatus('音声認識スタート')
+})
